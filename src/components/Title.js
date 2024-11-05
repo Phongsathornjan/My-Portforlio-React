5395b76185d6fa5f62163079f6f1b234548a0e16
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2024-11-05 23:27:52 +0700

    edit icon

--- src/components/Title.js
+++ src/components/Title.js
@@ -20,7 +20,7 @@ export const Title = () => {
             <div className="d-flex">
               <p id="text-hover" style={{fontSize:'18px',marginRight: '20px'}}><MdOutlineEmail /> Email : Phongsathornjanjamsai@gmail.com</p>
               <a href="https://www.facebook.com/profile.php?id=100007374097910" target="_blank" rel="noreferrer"><img src={`${process.env.PUBLIC_URL}/icons/facebook-icon.png`} alt="facebook" style={{width: '45px',height: '42px'}} id="icon-hover"/></a>
-              <a href="https://github.com/Phongsathornjan" target="_blank" rel="noreferrer" className="ms-3" style={{color: '#fff'}}><img src={`${process.env.PUBLIC_URL}/icons/Github_icon.png`} alt="github" style={{width: '40px',height: '42px',marginTop : '4px'}} id="icon-hover"/></a>
+              <a href="https://github.com/Phongsathornjan" target="_blank" rel="noreferrer"><img src={`${process.env.PUBLIC_URL}/icons/github_icon.png`} alt="github" style={{width: '45px',height: '42px',marginLeft: '20px'}} id="icon-hover"/></a>
             </div>
           </div>
         </div>
